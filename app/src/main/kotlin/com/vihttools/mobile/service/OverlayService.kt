19e12ca508835c82f995f1755e50fb10377cf73a
AndreyViht <anviht@yandex.ru>
2026-04-27 12:04:50 +0300

    Import kotlinx.coroutines.cancel in OverlayService
    Added import for kotlinx.coroutines.cancel to OverlayService.

--- app/src/main/kotlin/com/vihttools/mobile/service/OverlayService.kt
+++ app/src/main/kotlin/com/vihttools/mobile/service/OverlayService.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.cancel
 
 class OverlayService : Service() {
 
