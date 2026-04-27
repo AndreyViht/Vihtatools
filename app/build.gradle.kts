e92387ba21a1b153a54ade958701bf71b80b41a1
AndreyViht <anviht@yandex.ru>
2026-04-27 12:48:49 +0300

    Add kotlinx-coroutines-play-services dependency
    Added kotlinx-coroutines-play-services dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
     // Core
     implementation("androidx.core:core-ktx:1.12.0")
